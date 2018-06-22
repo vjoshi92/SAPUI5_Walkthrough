497ef77b5fe2c833cd112b2b156117153417d2ae
P1940969024 <vijayjoshi.90@gmail.com>
2018-06-22 10:17:59 +0000

    Step 6: Modules - commiting changes

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function(Controller,MessageToast) {
 	"use strict";
 
 	return Controller.extend("com.synconic.practice.SAPUI5_Walkthorugh.controller.Home", {
 		onPressHello: function(){
-			alert("Hello World");
+			MessageToast.show("Hello World");
 		}
 	});
 });
