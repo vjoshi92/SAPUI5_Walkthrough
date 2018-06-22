905288d71595c72bccc5a8a08e4b0233bfa67a89
P1940969024 <vijayjoshi.90@gmail.com>
2018-06-22 10:26:29 +0000

    Step 7: JSON Model - commiting changes

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,10 +1,21 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function(Controller,MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel"
+], function(Controller,MessageToast,JSONModel) {
 	"use strict";
 
 	return Controller.extend("com.synconic.practice.SAPUI5_Walkthorugh.controller.Home", {
+		
+		onInit:function(){
+			var oData = {
+				recipient : {
+					name:"World"
+				}	
+			};
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+		},
 		onPressHello: function(){
 			MessageToast.show("Hello World");
 		}
