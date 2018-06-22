8327227f7f7a9f80981ef2d80545572bd21b61dd
P1940969024 <vijayjoshi.90@gmail.com>
2018-06-22 14:48:40 +0000

    Step 9: Component Configuration - changes commited

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,14 +1,22 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
-	"com/synconic/practice/SAPUI5_Walkthorugh/model/models"
-], function(UIComponent, Device, models) {
+	"com/synconic/practice/SAPUI5_Walkthorugh/model/models",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function(UIComponent, Device, models,JSONModel,ResourceModel) {
 	"use strict";
 
 	return UIComponent.extend("com.synconic.practice.SAPUI5_Walkthorugh.Component", {
 
 		metadata: {
-			manifest: "json"
+			//manifest: "json"
+			rootView:{
+				"viewName": "com.synconic.practice.SAPUI5_Walkthorugh.view.Home",
+				"type": "XML",
+				"async": true,
+				"id": "app"
+			}
 		},
 
 		/**
@@ -20,11 +28,23 @@ sap.ui.define([
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
-			// enable routing
-			this.getRouter().initialize();
+			var oData = { recipient : {
+				name:"World"
+				}
+			};
+			
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+			
+			var i18nModel = new ResourceModel({
+				bundleName: "com.synconic.practice.SAPUI5_Walkthorugh.i18n.i18n"
+			});
+			this.setModel(i18nModel,"i18n");
+			// // enable routing
+			// this.getRouter().initialize();
 
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
+			// // set the device model
+			// this.setModel(models.createDeviceModel(), "device");
 		}
 	});
 });
--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,29 +1,13 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel"
+	"sap/m/MessageToast"
 ], function(Controller,MessageToast,JSONModel,ResourceModel) {
 	"use strict";
 
 	return Controller.extend("com.synconic.practice.SAPUI5_Walkthorugh.controller.Home", {
 		
-		onInit:function(){
-			var oData = {
-				recipient : {
-					name:"World"
-				}	
-			};
-			var oModel = new JSONModel(oData);
-			this.getView().setModel(oModel);
-			
-			var i18nModel = new ResourceModel({
-            bundleName: "com.synconic.practice.SAPUI5_Walkthorugh.i18n.i18n"
-        	});
-        	this.getView().setModel(i18nModel, "i18n1");
-		},
 		onPressHello: function(){
-			var oBundle = this.getView().getModel("i18n1").getResourceBundle();
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
 			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
 			var sMSg = oBundle.getText("helloMsg",[sRecipient]);
 			
