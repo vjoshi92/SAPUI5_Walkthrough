f6e97b131b603620bce89e5db72908d1ace299c9
P1940969024 <vijayjoshi.90@gmail.com>
2018-06-22 11:11:27 +0000

    Step 8: Translatable Texts - commiting changes

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel"
-], function(Controller,MessageToast,JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function(Controller,MessageToast,JSONModel,ResourceModel) {
 	"use strict";
 
 	return Controller.extend("com.synconic.practice.SAPUI5_Walkthorugh.controller.Home", {
@@ -15,9 +16,18 @@ sap.ui.define([
 			};
 			var oModel = new JSONModel(oData);
 			this.getView().setModel(oModel);
+			
+			var i18nModel = new ResourceModel({
+            bundleName: "com.synconic.practice.SAPUI5_Walkthorugh.i18n.i18n"
+        	});
+        	this.getView().setModel(i18nModel, "i18n1");
 		},
 		onPressHello: function(){
-			MessageToast.show("Hello World");
+			var oBundle = this.getView().getModel("i18n1").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMSg = oBundle.getText("helloMsg",[sRecipient]);
+			
+			MessageToast.show(sMSg);
 		}
 	});
 });
